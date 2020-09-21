e60de498f4bda22af85087bf338ec257895239b3
cfaulkner985 <cfaulkner985@gmail.com>
2020-09-21 18:26:21 +0000

    Added javascript for sort funtionality and added more comments

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+""" Paths to each page on the site """
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
