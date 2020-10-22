077b4aa733e6038aa06060953530ca27329e0c3a
cfaulkner985 <cfaulkner985@gmail.com>
2020-10-22 17:27:01 +0000

    Fixed spelling mistakes in 404 files

--- cart/views.py
+++ cart/views.py
@@ -25,7 +25,7 @@ def add_to_cart(request, item_id):
         will be moved to the cart. I have decided to keep size in
         as I will be using it in the future
     """
-    raise Exception("ABC")
+    
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
